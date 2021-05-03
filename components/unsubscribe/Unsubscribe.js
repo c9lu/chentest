977d1c8d78c32df0a81419b996060cce2c8e951f
c_lu <c_lu@oxfordproperties.com>
2021-05-03 07:58:13 -0400

    commit

--- components/unsubscribe/Unsubscribe.js
+++ components/unsubscribe/Unsubscribe.js
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from './Unsubscribe.module.css';
+import { renderHTML } from 'agility/utils'
+
 const getQueryStringValue=(key)=>{
     
     console.log(key);
@@ -13,33 +15,43 @@ const Unsubscribe = ({unsubscribeCompletedMessage, oxfordLogo,  yorkdaleLogo, sq
     
    // this.setState({completed:false})
     const [completed, setCompleted] = useState(false);
-
+    const homePage = document.getElementById("homePage");
     if(orgid=="402"){
         imageSource = yorkdaleLogo;
+        homePage?homePage.href="https://yorkdale.com/":'#';
     }
     else if(orgid=="403"){
         imageSource = upperCanadaLogo;
+        homePage?homePage.href="https://uppercanadamall.com/":'#';
     }
     else if(orgid=="404"){
 
         imageSource = squareOneLogo;
+        homePage?homePage.href="https://shopsquareone.com/":'#';
     }
     let email = getQueryStringValue("email").toString();
+    let websiteUrl = '';
     
+    useEffect(()=>{
+        document.getElementById('emailAdress').innerText = email;
+       
+      
+    },[]);
     return (
     
-    <div className={styles.container} role="main">
+    <div>
         {completed==false? 
-        <div >        
+        <div className={styles.container} role="main">        
             <img src={imageSource.url} className={styles.logo}/>
-            <div className={styles.introMessage}>{introMessage}</div>  
+            <div className={styles.introMessage} dangerouslySetInnerHTML={renderHTML(introMessage)}/>  
             
             <br/>
           
             <button type="button" className={styles.colorButton} onClick={()=>setCompleted(true)}>Unsubscribe</button> <br/>
         </div>:null}
-        {completed==true?<div dangerouslySetInnerHTML={{__html:unsubscribeCompletedMessage}}/>:null}
         
+        <div style={{display:completed==true?"block":"none"}}><div className={styles.container} role="main"><div dangerouslySetInnerHTML={{__html:unsubscribeCompletedMessage}}/> </div>
+        </div>
     </div>
         
        
