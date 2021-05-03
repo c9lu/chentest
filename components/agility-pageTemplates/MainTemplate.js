02b21562c6419e7c6b7ddf7a7df33e5ba43c16ea
c_lu <c_lu@oxfordproperties.com>
2021-05-03 11:59:29 -0400

    commit

--- components/agility-pageTemplates/MainTemplate.js
+++ components/agility-pageTemplates/MainTemplate.js
@@ -5,7 +5,7 @@ class OneColumnTemplate extends Component {
     render() {
         return (
         <div className="one-column-template">
-          <ContentZone name='MainContentZone' {...this.props} />
+         {/* <ContentZone name='MainContentZone' {...this.props} /> */}
           <ContentZone name='UnsubscribeContentZone' {...this.props}/>
         </div>
         );
